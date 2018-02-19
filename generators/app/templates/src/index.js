39268a6833503bdc9a0ff6896dbd8de2aa068453
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2018-02-19 15:10:05 -0300

    Adiciona configuracao da store

--- generators/app/templates/src/index.js
+++ generators/app/templates/src/index.js
@@ -1,10 +1,19 @@
 import dotenv from 'dotenv'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from 'store'
 import registerServiceWorker from './registerServiceWorker'
 import App from './App'
 
 dotenv.config()
+const store = configureStore()
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const Application = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(<Application />, document.getElementById('app'))
 registerServiceWorker()
